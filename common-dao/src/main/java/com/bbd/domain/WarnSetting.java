cec0f1902b0aacf1ad841581a9148e32aa080806
804912547@qq.com <804912547@qq.com>
2017-10-24 16:06:10 +0800

    系统配置逆向工程

---
+++ common-dao/src/main/java/com/bbd/domain/WarnSetting.java
@@ -0,0 +1,95 @@
+package com.bbd.domain;
+
+import java.util.Date;
+
+public class WarnSetting {
+    private Long id;
+
+    private String name;
+
+    private Integer type;
+
+    private Integer min;
+
+    private Integer max;
+
+    private Long createBy;
+
+    private Date gmtCreate;
+
+    private Long modifiedBy;
+
+    private Date gmtModified;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Integer getType() {
+        return type;
+    }
+
+    public void setType(Integer type) {
+        this.type = type;
+    }
+
+    public Integer getMin() {
+        return min;
+    }
+
+    public void setMin(Integer min) {
+        this.min = min;
+    }
+
+    public Integer getMax() {
+        return max;
+    }
+
+    public void setMax(Integer max) {
+        this.max = max;
+    }
+
+    public Long getCreateBy() {
+        return createBy;
+    }
+
+    public void setCreateBy(Long createBy) {
+        this.createBy = createBy;
+    }
+
+    public Date getGmtCreate() {
+        return gmtCreate;
+    }
+
+    public void setGmtCreate(Date gmtCreate) {
+        this.gmtCreate = gmtCreate;
+    }
+
+    public Long getModifiedBy() {
+        return modifiedBy;
+    }
+
+    public void setModifiedBy(Long modifiedBy) {
+        this.modifiedBy = modifiedBy;
+    }
+
+    public Date getGmtModified() {
+        return gmtModified;
+    }
+
+    public void setGmtModified(Date gmtModified) {
+        this.gmtModified = gmtModified;
+    }
+}
