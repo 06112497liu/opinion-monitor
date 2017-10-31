82a8a2a9124abb7e4554d25e891708bec7ae5195
wangtianjing <wangtianjing@bbdservice.com>
2017-10-31 09:54:50 +0800

    ES代码开发

---
+++ web/src/main/java/com/bbd/service/StatisticService.java
@@ -0,0 +1,13 @@
+/**
+ * BBD Service Inc
+ * All Rights Reserved @2016
+ */
+package com.bbd.service;
+
+/**
+ *
+ * @author tjwang
+ * @version $Id: StatisticService.java, v 0.1 2017/10/31 0031 9:53 tjwang Exp $
+ */
+public class StatisticService {
+}
