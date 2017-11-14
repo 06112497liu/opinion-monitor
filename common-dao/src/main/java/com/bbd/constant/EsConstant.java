ff2083adb6135acdf02bb0b29836404074f00612
wangtianjing <wangtianjing@bbdservice.com>
2017-11-14 11:22:48 +0800

    代码合并

--- common-dao/src/main/java/com/bbd/constant/EsConstant.java
+++ common-dao/src/main/java/com/bbd/constant/EsConstant.java
@@ -10,19 +10,21 @@ package com.bbd.constant;
  */
 public class EsConstant {
 
-    public static final String LONG_TIME_FORMAT = "yyyy-MM-dd HH:mm:ss";
+    public static final String LONG_TIME_FORMAT               = "yyyy-MM-dd HH:mm:ss";
 
     //    public static final String IDX_OPINION             = "bbd_opinion_mock_a";
-    public static final String IDX_OPINION             = "bbd_opinion_mock_a";
-    public static final String OPINION_TYPE            = "opinion";
-    public static final String OPINION_OP_RECORD_TYPE  = "opinion_op_record";
-    public static final String IDX_OPINION_HOT = "bbd_opinion_hot";
-    public static final String OPINION_HOT_TYPE = "hot";
-    public static final String OPINION_UUID            = "uuid";
-    public static final String OPINION_HOT_PROP        = "hot";
-    public static final String OPINION_FIRST_WARN_TIME = "firstWarnTime";
+    public static final String IDX_OPINION                    = "bbd_opinion_mock_a";
+    public static final String OPINION_TYPE                   = "opinion";
+    public static final String OPINION_OP_RECORD_TYPE         = "opinion_op_record";
+    public static final String IDX_OPINION_HOT                = "bbd_opinion_hot";
+    public static final String OPINION_HOT_TYPE               = "hot";
+    public static final String OPINION_UUID                   = "uuid";
+    public static final String OPINION_HOT_PROP               = "hot";
+    public static final String OPINION_FIRST_WARN_TIME        = "firstWarnTime";
 
     // =========== 舆情操作记录字段 ==========================
+
+    public static final String IDX_OPINION_OP_RECORD          = "bbd_opinion_op_record";
     public static final String opOwnerField                   = "opOwner";
     public static final String opStatusField                  = "opStatus";
     public static final String targeterField                  = "targeter";
@@ -42,7 +44,7 @@ public class EsConstant {
     public static final String eventsField                    = "events";
     public static final String mediaTypeField                 = "mediaType";
     public static final String emotionField                   = "emotion";
-    public static final String firstWarnTimeField             = "firstWarnTime";
+    public static final String warnTimeField                  = "warnTime";
     public static final String websiteField                   = "website";
     public static final String keywordField                   = "keyword";
 
