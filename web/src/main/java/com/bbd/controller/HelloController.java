e33334896aa1e76b3a953bc1d87eefd76327a3b1
804912547@qq.com <804912547@qq.com>
2017-11-16 18:21:25 +0800

    注释

--- web/src/main/java/com/bbd/controller/HelloController.java
+++ web/src/main/java/com/bbd/controller/HelloController.java
@@ -6,6 +6,8 @@ package com.bbd.controller;
 
 import com.bbd.RestResult;
 import com.bbd.annotation.CheckAuth;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiParam;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
@@ -17,6 +19,7 @@ import org.springframework.web.bind.annotation.RestController;
  */
 @RestController
 @RequestMapping("/api/hello")
+@Api(description = "接口测试")
 public class HelloController extends AbstractController {
 
     @CheckAuth(permission = "world")
