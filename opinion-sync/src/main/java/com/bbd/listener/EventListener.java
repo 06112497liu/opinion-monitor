c6b6f0102d93a4633a7d68bcfb42ace55b6c5519
daijinlong <djl_20062006@126.com>
2017-11-16 17:06:47 +0800

    增加事件热度和词云更新功能

---
+++ opinion-sync/src/main/java/com/bbd/listener/EventListener.java
@@ -0,0 +1,67 @@
+/**
+ * BBD Service Inc
+ * All Rights Reserved @2016
+ */
+package com.bbd.listener;
+
+import java.util.Date;
+import java.util.List;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.stereotype.Component;
+
+import com.alibaba.fastjson.JSON;
+import com.bbd.dao.OpinionEventDao;
+import com.bbd.dao.OpinionEventWordsDao;
+import com.bbd.domain.OpinionEvent;
+import com.bbd.domain.OpinionEventWords;
+import com.bbd.domain.OpinionEventWordsExample;
+
+/** 
+ * @author daijinlong 
+ * @version $Id: EventService.java, v 0.1 2017年11月10日 下午2:06:26 daijinlong Exp $ 
+ */
+@Component
+public class EventListener {
+
+    private static final Logger logger = LoggerFactory.getLogger(EventListener.class);
+
+    @Autowired
+    private OpinionEventDao opinionEventDao;
+    @Autowired
+    private OpinionEventWordsDao opinionEventWordsDao;
+
+    @KafkaListener(topics = "bbd_event_words", containerFactory = "kafkaListenerContainerFactory")
+    public void ListenWords(String record) {
+        long start = System.currentTimeMillis();
+        List<OpinionEventWords> opinionEventWords = JSON.parseArray(record, OpinionEventWords.class);
+        for (OpinionEventWords opinionEventWord : opinionEventWords) {
+            OpinionEventWordsExample example = new OpinionEventWordsExample();
+            example.createCriteria().andEventIdEqualTo(opinionEventWord.getEventId()).andCycleEqualTo(opinionEventWord.getCycle());
+            List<OpinionEventWords>  opinionEventWordsList= opinionEventWordsDao.selectByExample(example);
+            if (opinionEventWordsList != null && opinionEventWordsList.size() > 0) {
+                opinionEventWord.setGmtModified(new Date());
+                opinionEventWordsDao.updateByPrimaryKeySelective(opinionEventWord);
+            } else {
+                opinionEventWord.setGmtCreate(new Date());
+                opinionEventWordsDao.insert(opinionEventWord);
+            }
+        }
+        long end = System.currentTimeMillis();
+        logger.info("Process {} wors success, time used: {}", opinionEventWords.size(), end - start);
+    }
+    @KafkaListener(topics = "bbd_event_hot", containerFactory = "kafkaListenerContainerFactory")
+    public void ListenHot(String record) {
+        long start = System.currentTimeMillis();
+        List<OpinionEvent> opinionEvents = JSON.parseArray(record, OpinionEvent.class);
+        for (OpinionEvent opinionEvent : opinionEvents) {
+            opinionEvent.setGmtCreate(new Date());
+            opinionEventDao.updateByPrimaryKeySelective(opinionEvent);
+        }
+        long end = System.currentTimeMillis();
+        logger.info("Process {} eventHot success, time used: {}", opinionEvents.size(), end - start);
+    }
+}
