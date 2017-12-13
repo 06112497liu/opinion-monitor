a15a4f9e4a23837a04f2edf03f563608a889c21e
804912547@qq.com <804912547@qq.com>
2017-12-13 18:00:46 +0800

    bug修改

--- opinion-sync/src/main/java/com/bbd/listener/OpinionNewsListener.java
+++ opinion-sync/src/main/java/com/bbd/listener/OpinionNewsListener.java
@@ -80,9 +80,9 @@ public class OpinionNewsListener {
 
         BulkRequestBuilder bulk = client.prepareBulk();
         for (OpinionNewsVO vo : vos) {
-            String id = UUID.randomUUID().toString();
-            //String id = vo.getId();
-            System.out.println("id: " + vo.getId());
+            //String id = UUID.randomUUID().toString();
+            String id = vo.getId();
+            //System.out.println("id: " + vo.getId());
             IndexRequest ir = new IndexRequest(index, type, id);
             ir.source(JsonUtil.fromJson(vo), XContentType.JSON);
             UpdateRequest ur = new UpdateRequest(index, type, id).upsert(ir);
