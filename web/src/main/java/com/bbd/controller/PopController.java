d8e7dacb19d0c59c4cc89d04c35c47971f767a3e
daijinlong <djl_20062006@126.com>
2018-01-16 14:38:12 +0800

    test冲突

--- web/src/main/java/com/bbd/controller/PopController.java
+++ web/src/main/java/com/bbd/controller/PopController.java
@@ -37,7 +37,7 @@ public class PopController extends AbstractController {
         Long userId = UserContext.getUser().getId();
         HashMap map = new HashMap();
         map.put("eventNewOpinion", msgService.getPop(userId, 2));
-        map.put("000", "000");
+        map.put("111", "111");
         map.put("eventLevelChange", msgService.getPop(userId, 3));
         return RestResult.ok(map);
     }
